0033a38f5534f82d45193129f776e855c29e35f2
Sezinando Vieira <sezinando.vieira@gmail.com>
2025-06-30 10:02:17 -0300

    Adicionando modificações aos comandos personalizaveis

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,14 @@
-Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', (data = {
+    firtsname: 'Clovis',
+    lastName: 'Barros',
+    email: 'clovis@barros.com.br',
+}) => {
 
-    cy.get('[id=firstName]').type('Sezinando', { delay:0 });
+    cy.get('[id=firstName]').type(data.firtsname, { delay:0 });
 
-    cy.get('[id=lastName]').type('Vieira', { delay:0 });
+    cy.get('[id=lastName]').type(data.lastName, { delay:0 });
        
-    cy.get('[id=email]').type('teste@teste.com', { delay:0 });
+    cy.get('[id=email]').type(data.email, { delay:0 });
 
     const texto = Cypress._.repeat('Esse texto, muito texto, cheio de texto, que é texto', 7)
     cy.get('[id=open-text-area]').type(texto, { delay:0 })
